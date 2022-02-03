ebcefbd93f396f8b151639d9ff7b5e6bfcf3a233
Wade Dorrell <wade@dorrells.org>
2022-02-03 10:32:47 -0700

    Round coordinates (in the last update they changed needlessly)

--- update.js
+++ update.js
@@ -4,17 +4,19 @@ const csv_parse = require('csv-parse/lib/sync')
 // https://city-of-boise.opendata.arcgis.com/datasets/parks-and-recreation-managed-parks-and-reserves-point/data?orderBy=ParkID
 const url = 'https://opendata.arcgis.com/datasets/a0499e28888a44548c52d6e9a2d21b48_0.csv';
 
+const toCoord = (numericString) => Number.parseFloat(numericString).toFixed(4);
+
 const process = async url => {
     const response = await fetch(url);
     const csv = await response.text();
     var trimmedCsv = csv.trim();
-    var parsed = csv_parse(trimmedCsv, {columns: true});
+    var parsed = csv_parse(trimmedCsv, { columns: true });
     var mapped = parsed.map(v => {
         return {
             // e.g. Sue Howell Park has no ParkID
             'id': v["ParkID"].length > 0 ? v["ParkID"] : v["Site_Name"],
-            'lat': Number.parseFloat(v["Y"]),
-            'long': Number.parseFloat(v["X"]),
+            'lat': toCoord(v["Y"]),
+            'long': toCoord(v["X"]),
             'name': v["Site_Name"],
             'type': v["Park_Type"],
             'park_status': v["Park_Status"],
